aa178e3efdb087c23f736e76412866da5505b840
Zonglei Jiao <jiaozonglei@gmail.com>
2012-02-03 12:36:20 +1100

    new class of property

---
+++ src/edu/monash/infotech/OWLProperty.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.monash.infotech;
+
+/**
+ *
+ * @author Zonglei Jiao
+ */
+public class OWLProperty {
+    
+    public String propertyType;
+    public String propertyName;
+    public String propertyDomain;
+    public String propertyRange;    
+}
